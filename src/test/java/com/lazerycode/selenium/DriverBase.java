f56163433226e141c838f79bf87c1556f777a466
Shamsul  Bhuiyan <nakibcd7@gmail.com>
2018-06-18 23:09:58 -0400

    forkcommit

--- src/test/java/com/lazerycode/selenium/DriverBase.java
+++ src/test/java/com/lazerycode/selenium/DriverBase.java
@@ -1,49 +1,38 @@
-package com.lazerycode.selenium;
-
-import com.lazerycode.selenium.config.DriverFactory;
-import com.lazerycode.selenium.listeners.ScreenshotListener;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Listeners;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-@Listeners(ScreenshotListener.class)
-public class DriverBase {
-
-    private static List<DriverFactory> webDriverThreadPool = Collections.synchronizedList(new ArrayList<DriverFactory>());
-    private static ThreadLocal<DriverFactory> driverFactoryThread;
-
-    @BeforeSuite(alwaysRun = true)
-    public static void instantiateDriverObject() {
-        driverFactoryThread = ThreadLocal.withInitial(() -> {
-            DriverFactory driverFactory = new DriverFactory();
-            webDriverThreadPool.add(driverFactory);
-            return driverFactory;
-        });
-    }
-
-    public static RemoteWebDriver getDriver() throws Exception {
-        return driverFactoryThread.get().getDriver();
-    }
-
-    @AfterMethod(alwaysRun = true)
-    public static void clearCookies() {
-        try {
-            driverFactoryThread.get().getStoredDriver().manage().deleteAllCookies();
-        } catch (Exception ignored) {
-            System.out.println("Unable to clear cookies, driver object is not viable...");
-        }
-    }
-
-    @AfterSuite(alwaysRun = true)
-    public static void closeDriverObjects() {
-        for (DriverFactory driverFactory : webDriverThreadPool) {
-            driverFactory.quitDriver();
-        }
-    }
+package com.w2a.testcases;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class testngexmp {
+        String driverPath="C:/Usb/";
+		WebDriver driver;
+		String baseUrl;
+		@BeforeTest
+		public void setUp() throws Exception {
+			System.out.println("Launching Firefox browser..");
+			System.setProperty("webdriver.gecko.driver",driverPath+"geckodriver.exe"); 
+			driver = new FirefoxDriver();
+			baseUrl = "http://letskodeit.teachable.com/";
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+		}
+		@Test
+		public void test() {
+			driver.get(baseUrl);
+			driver.findElement(By.xpath("//div[@id='navbar']//a[@href='/sign_in']")).click();
+			System.out.println("Clicked on login");
+			driver.findElement(By.id("user_email")).sendKeys("test@email.com");
+			System.out.println("Sending keys to username field");
+			driver.findElement(By.id("user_password")).sendKeys("test");
+			System.out.println("Sending keys to password field");
+			driver.findElement(By.id("user_email")).clear();
+			System.out.println("Clearing the username field");
+		}
+		@AfterTest
+		public void tearDown() throws Exception {
+			 driver.quit();
+		}
 }
